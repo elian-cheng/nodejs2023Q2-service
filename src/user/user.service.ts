b2de1d2c0c3fc3f941d354a448721ba45d3b8d61
Olga Chernega <eliang.cheng@gmail.com>
2023-08-19 14:17:00 +0300

    feat: update user service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,14 +3,16 @@ import {
   Injectable,
   NotFoundException,
 } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ConfigService } from '@nestjs/config';
 import { IUserData, prepareUserResponse } from './models/user.model';
 import { CreateUserDTO } from './dto/createUser.dto';
 import { UpdatePasswordDTO } from './dto/updateUser.dto';
 
 @Injectable()
 export class UserService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService, private config: ConfigService) {}
 
   async getUsers(): Promise<IUserData | IUserData[]> {
     const users = await this.prisma.user.findMany();
@@ -29,10 +31,13 @@ export class UserService {
   }
 
   async createUser(dto: CreateUserDTO): Promise<IUserData | IUserData[]> {
+    const salt = Number(this.config.get('CRYPT_SALT'));
+    const hash = await bcrypt.hash(dto.password, salt);
     const user = await this.prisma.user.create({
       data: {
         login: dto.login,
-        password: dto.password,
+        password: hash,
+        hashedRt: '',
         version: 1,
       },
     });
@@ -49,11 +54,20 @@ export class UserService {
     });
 
     if (!response) throw new NotFoundException();
-    if (response.password !== dto.oldPassword) throw new ForbiddenException();
+
+    const isPasswordMatches = await bcrypt.compare(
+      dto.oldPassword,
+      response.password,
+    );
+
+    if (!isPasswordMatches) throw new ForbiddenException();
+
+    const salt = Number(this.config.get('CRYPT_SALT'));
+    const hash = await bcrypt.hash(dto.newPassword, salt);
 
     const updatedUser = await this.prisma.user.update({
       where: { id: userId },
-      data: { password: dto.newPassword, version: (response.version += 1) },
+      data: { password: hash, version: (response.version += 1) },
     });
 
     return prepareUserResponse(updatedUser);
@@ -61,10 +75,6 @@ export class UserService {
 
   async deleteUser(userId: string) {
     try {
-      const user = await this.prisma.user.findUnique({ where: { id: userId } });
-      if (!user) {
-        throw new NotFoundException(`User with id ${userId} not found`);
-      }
       await this.prisma.user.delete({ where: { id: userId } });
     } catch (error) {
       throw error;
